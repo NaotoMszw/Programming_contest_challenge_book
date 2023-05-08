b164990ee0277cd6cfdc17bbab3ade6f51c45eb2
NaotoMszw <e92mszw@gmail.com>
2023-05-08 22:57:31 +0900

    cable_master.h

--- 3-1/cable_master.h
+++ 3-1/cable_master.h
@@ -1,6 +1,37 @@
 #include <vector>
+#include <float.h>
 
-int solve_cable_master(){
+bool judge(std::vector<float> y, int k, float l){
 
+    int num = 0;
+    for(int i = 0; i < y.size(); i++){
 
+        num += (int)(y[i] / l);
+    }
+
+    return num >= k;
+}
+
+int solve_cable_master(std::vector<float> y, int k){
+
+    constexpr double e = 0.001;
+
+    float lb = 0.0;
+    float ub = FLT_MAX;
+
+    while(ub - lb > e){
+
+        std::cout << ub << " " << lb << std::endl;
+        float mid = (lb + ub) / 2.0;
+        if(judge(y, k, mid)){
+
+            lb = mid;
+        }
+        else{
+
+            ub = mid;
+        }
+    }
+
+    return floor(ub * 100) / 100;
 }
--- 3-1/main.cpp
+++ 3-1/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
 #include "lower_bound.h"
+#include "cable_master.h"
 
 int main(){
 
-    std::vector<int> y = {2, 3, 3, 5, 6};
+    std::vector<int> int_y = {2, 3, 3, 5, 6};
     int k = 3;
 
-    int answer = solve_lower_bound(y, k);
-    std::cout << answer << std::endl;
+    int i_answer = solve_lower_bound(int_y, k);
+    std::cout << i_answer << std::endl;
+
+    std::vector<float> float_y = {8.02, 7.43, 4.57, 5.39};
+    k = 11;
+    float f_answer = solve_cable_master(float_y, k);
+    std::cout << "Answer cable master: " << f_answer << std::endl;
 }
